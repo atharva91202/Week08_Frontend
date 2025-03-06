ee8edd3df87d904c43c7942fae58a44de2745604
Atharva <atharvasharma.cs25@gmail.com>
2025-03-06 23:19:23 +0530

    [Atharva] Add. Find min and max of 5 random 3 digit number

---
+++ src/main/javascript/selectionpractise/maxandMin.js
@@ -0,0 +1,21 @@
+let numbers = [];
+for (let i = 0; i < 5; i++) {
+    let num = Math.floor(Math.random() * 900) + 100; // Random 3-digit number (100 to 999)
+    numbers.push(num);
+}
+console.log("Generated Numbers:", numbers);
+
+let min = numbers[0];
+let max = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < min) {
+        min = numbers[i];
+    }
+    if (numbers[i] > max) {
+        max = numbers[i];
+    }
+}
+
+console.log("Minimum Value:", min);
+console.log("Maximum Value:", max);
